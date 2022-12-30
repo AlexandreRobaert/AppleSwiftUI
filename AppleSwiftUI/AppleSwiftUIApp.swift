fece2a0999b32dc1909470c9bf053f2cb4071900
Alexandre Robaert <Robaert.developer@icloud.com>
2022-12-30 19:48:21 -0300

    Initial Commit

---
+++ AppleSwiftUI/AppleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSwiftUIApp.swift
+//  AppleSwiftUI
+//
+//  Created by Alexandre Robaert on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AppleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
